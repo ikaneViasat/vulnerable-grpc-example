1bd0cd2c59abecb4d0a522c93cebc709f8b7eb55
ikane <ian.kane@viasat.com>
2022-06-10 15:02:10 -0700

    Add licensing

--- cpp/helloworld/greeter_server.cc
+++ cpp/helloworld/greeter_server.cc
@@ -14,6 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * NOTICE: THIS FILE WAS MODIFIED FROM GRPC REPOSITORY by ikane
+ *
  */
 
 #include <iostream>
